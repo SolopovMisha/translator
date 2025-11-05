295f0526c01dae4138da3e4acb3754078a3f912b
Bezdar <100799406+SolopovMisha@users.noreply.github.com>
2025-11-05 19:55:53 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,9 @@ from googletrans import LANGUAGES
 
 translator = Translator()
 
-
-
-
 print(f"""Список доступных языков:
 {LANGUAGES}""")
 selected_language = input("Выберите 1 из доступных языков: ")
-
-
-
 
 title = "Music’s Impact and Influence on Everyday Life"
 text = """As a person who loves music and even got into playing an instrument myself, it does have an influence on my everyday life. I’m constantly listening to music whether it be while working during class, in the car on my way home, or just while practicing playing the guitar. Music is all around the world and one of the most popular forms of entertainment and escape. It consists of many different styles and opinions, whether it be just personal opinion or even a part of your culture.
